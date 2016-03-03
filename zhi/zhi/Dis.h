610b7a202bcd79e2c24580ac1fe681ebd6896ca2
cc <cc@qq.com>
2016-03-03 15:04:14 +0800

    1

---
+++ zhi/zhi/Dis.h
@@ -0,0 +1,13 @@
+//
+//  Dis.h
+//  zhi
+//
+//  Created by cloud 唐 on 16/3/3.
+//  Copyright © 2016年 imCloud. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dis : NSObject
+
+@end
---
+++ zhi/zhi/Dis.m
@@ -0,0 +1,13 @@
+//
+//  Dis.m
+//  zhi
+//
+//  Created by cloud 唐 on 16/3/3.
+//  Copyright © 2016年 imCloud. All rights reserved.
+//
+
+#import "Dis.h"
+
+@implementation Dis
+
+@end
